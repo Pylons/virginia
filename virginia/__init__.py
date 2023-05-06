657d8b4a3cde6385f90946484942b2ce69b6a88c
Sergiu Bivol <sergiu@cip.md>
2023-05-06 01:59:24 +0100

    Update formatting

--- virginia/__init__.py
+++ virginia/__init__.py
@@ -2,17 +2,18 @@ import os
 
 from pyramid.config import Configurator
 
-from virginia.models import Directory
-from virginia.models import Filesystem
+from virginia.models import Directory, Filesystem
+
 
 def main(global_config, **settings):
-    root = settings.pop('root', None)
+    root = settings.pop("root", None)
     if root is None:
-        raise ValueError('virginia requires a root')
+        raise ValueError("virginia requires a root")
     fs = Filesystem(os.path.abspath(os.path.normpath(root)))
+
     def get_root(environ):
         return Directory(fs, root)
+
     config = Configurator(root_factory=get_root, settings=settings)
     config.scan()
     return config.make_wsgi_app()
-
